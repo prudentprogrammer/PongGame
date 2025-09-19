485d132e1344d96d4a8339ea97fbbaa857b302ad
Arjun Bharadwaj <arjunbharadwaj94@gmail.com>
2025-09-19 17:44:07 +0530

    Setup Paddles

--- PongGame/PongGame/GameView.swift
+++ PongGame/PongGame/GameView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct GameView : View {
     var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            
-            // Draw the circle
-            Circle().fill(Color.white).frame(width: 20, height: 20)
+        GeometryReader { geo in
+            let (W, H) = (geo.size.width, geo.size.height)
+            let (halfW, halfH) = (W / 2, H / 2)
+            ZStack {
+                Color.black.ignoresSafeArea()
+                
+                // center line
+                Rectangle().fill(.white.opacity(0.2)).frame(width: 2)
+                
+                // paddles
+                Rectangle().fill(.white).frame(width: 16, height: 100).offset(x: -halfW + 30, y: 0)
+                Rectangle().fill(.white).frame(width: 16, height: 100).offset(x: halfW - 30, y: 0)
+                
+                // ball
+                Circle().fill(.white).frame(width: 18, height: 18).offset(x: 0, y: 0)
+            }
         }
     }
 }
