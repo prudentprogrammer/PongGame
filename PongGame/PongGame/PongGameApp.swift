021c8b7f54ca7eefe5ad905df9045224dbd34bd8
Arjun Bharadwaj <arjunbharadwaj94@gmail.com>
2025-09-19 17:19:52 +0530

    Initial scaffold files

---
+++ PongGame/PongGame/PongGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongGameApp.swift
+//  PongGame
+//
+//  Created by Lakshmi Bharadwaj on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PongGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PongGame/PongGameTests/PongGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  PongGameTests.swift
+//  PongGameTests
+//
+//  Created by Lakshmi Bharadwaj on 9/19/25.
+//
+
+import Testing
+@testable import PongGame
+
+struct PongGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
